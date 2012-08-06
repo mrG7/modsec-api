485a30b6dd69ace4f455df5a784a86c851656bad
Varrun Ramani <varrunr@gmail.com>
2012-08-06 01:33:53 -0700

    Compilation script for executable

---
+++ mod-security/standalone/build.sh
@@ -0,0 +1,7 @@
+STANDALONE_PATH=/home/varrunr/tomcat/github/modsec-api/mod-security/standalone
+JAVA_HOME=/home/varrunr/Downloads/jdk1.7.0_05
+APR_PATH=/usr/include/apr-1.0
+LIBXML_PATH=/usr/include/libxml2
+APACHE_PATH=/usr/include/apache2
+
+g++ main.cpp -I$STANDALONE_PATH -I$APACHE_PATH -I$APR_PATH -I$STANDALONE_PATH/../apache2 -I$LIBXML_PATH -L /usr/lib/ -L$STANDALONE_PATH -lstandalone -L$STANDALONE_PATH -lapr-1 -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -o modsec
